4c1f5c80cfffbe158d9a855ec62b3852182d54a2
deannaraelarson-hub <deannaraelarson@gmail.com>
2025-12-26 20:59:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/D/", 
+  base: "/D/", // Critical: GitHub Pages serves from /D/
   server: {
     port: 5173,
     strictPort: true
   }
 });
-
